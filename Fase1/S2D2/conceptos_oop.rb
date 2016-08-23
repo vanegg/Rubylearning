0da3fffa2a2269fce5d911645c45388812a4fcf3
codea_mac_04 <codea_mac_04@gmail.com>
2016-08-23 18:18:56 -0500

    terminologia add

---
+++ Fase1/S2D2/conceptos_oop.rb
@@ -0,0 +1,55 @@
+=begin
+De los términos siguientes explica en 2 o 3 oraciones cada concepto. Y ejemplifícalo con: dibujos, pseudo-código o código.
+
+Inheritance
+{
+  Forma para obtener funciones y métodos de otras clases 
+  Evitando repetir codigo  
+}
+
+Composition
+{
+  
+}
+Encapsulation
+{
+  
+}
+Duck Typing
+{
+  Desde que se cumpla con el contrato, no importa qué clase sea o qué otros atributos/métodos defina.
+}
+The Law of Demeter (ej.)
+Overriding Methods (and using super)
+{
+  Sobreescribir métodos en las subclases con el mismo nombre que en las superclases
+  Se utiliza super para llamar código de la superclase
+}
+Modules
+{
+  los módulos cumplen una doble función. Se usan para evitar colisiones en los nombres de las clases (namespaces) y como un mecanismo de reutilización de código (mixins). Los mixins son ideales para aquellas situaciones en que es necesario incluir (o mezclar) métodos en diferentes clases que no comparten nada en común.
+}
+Scope
+{
+  
+}
+Private vs Public Methods
+{
+  Los métodos públicos pueden ser accedidos desde cualquier parte de la aplicación.
+  Los métodos privados y protegidos pueden ser accedidos desde la misma clase o desde alguna de sus subclases
+}
+
+Instance vs Class methods and variables
+{
+  Metodos y variables que pueden llamarse por una instancia o por una clase. 
+}
+
+Polymorphism
+{
+  Formalmente se define como la capacidad de un objeto de tomar otra(s) forma(s)
+}
+Separation of Concerns
+{
+  Single Responsibility
+}
+=end
